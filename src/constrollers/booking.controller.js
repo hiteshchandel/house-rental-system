5c4d8407090a1a66c3f8d344f9981f46f21dd9b5
Hitesh Chandel <hiteshchandel858@gmail.com>
2025-08-08 01:18:53 +0530

    Implement booking functionality: add booking routes and controller methods for creating, retrieving, canceling, and completing bookings.

--- src/constrollers/booking.controller.js
+++ src/constrollers/booking.controller.js
@@ -1,7 +1,7 @@
 const Booking = require('../moduls/Booking.js');
 const Room = require('../moduls/Room.js');
 
-exports.bookRoom = async (req, res) => {
+exports.createBooking = async (req, res) => {
     try {
         const userId = req.user._id; 
         const { roomId } = req.body;
@@ -24,7 +24,7 @@ exports.bookRoom = async (req, res) => {
         room.isAvailable = false;
         await room.save();
 
-        return res.status.json({
+        return res.status(200).json({
             success: true,
             message: "Room booked SuccessFully",
             booking: newBooking
@@ -36,4 +36,119 @@ exports.bookRoom = async (req, res) => {
             message: "Internal server error"
         });
     };
+}
+
+exports.getAllBookings = async (req, res) => {
+    try {
+        const bookings = await Booking.find()
+            .populate('user', 'name email')
+            .populate({
+                path: 'room',
+                populate: { path: 'house', select: 'plotNumber city' }
+            })
+            .sort({ createdAt: -1 });
+        
+        return res.status(200).json({
+            success: true,
+            bookings
+        })
+    } catch (error) {
+        console.log("Error : ", error);
+        return res.status(500).json({
+            success: false,
+            message : "Internal server Error"
+        })
+    }
+}
+
+exports.getUserBookings = async (req, res) => {
+    try {
+        const userId = req.user._id;
+
+        const bookings = await Booking.find({ user: userId })
+            .populate('room')
+            .sort({ createdAt: -1 });
+        
+        return res.status(200).json({
+            success: true,
+            Bookings : bookings
+        })
+    } catch (error) {
+        console.log('Error : ', error);
+        return res.status(500).json({
+            success: false,
+            message: "Internal server Error"
+        })
+    }
+}
+
+
+exports.cancelBooking = async (req, res) => {
+    try {
+        const bookingId = req.params.id;
+
+        const booking = await Booking.findById(bookingId).populate('room');
+        if (!booking) {
+            return res.status(404).json({
+                success: false,
+                message: "Booking not found"
+            });
+        }
+
+        booking.status = 'cancelled';
+        await booking.save();
+
+        //Mark room as available again
+
+        const room = booking.room;
+        room.status = 'available';
+        room.isAvailable = true;
+        await room.save();
+
+        return res.status(200).json({
+            success: true,
+            message: "Booking cancelled successfully"
+        });
+    } catch (error) {
+        console.log('Error : ', error);
+        return res.status(500).json({
+            success: false,
+            message: "Internal server Error"
+        });
+    }
+}
+
+exports.completeBooking = async (req, res) => {
+    try {
+        const bookingId = req.params.id;
+
+        const booking = await Booking.findById(bookingId).populate("room");
+
+        if (!booking) {
+            return res.status(404).json({
+                success: false,
+                message: "Booking not found"
+            });
+        }
+
+        booking.status = 'completed';
+        await booking.save();
+
+        //Optional : mark room as available again
+        const room = booking.room;
+        room.status = 'available';
+        room.isAvailable = true;
+        await room.save();
+
+        return res.status(200).json({
+            success: true,
+            message: "Booking marked as completed"
+        });
+    } catch (error) {
+        console.log("Error :", error);
+        return res.status(500).json({
+            success: false,
+            message : "Internal server Error"
+        })
+    }
 }
---
+++ src/routes/booking.routes.js
@@ -0,0 +1,18 @@
+const router = require('express').Router();
+const { createBooking, getAllBookings, getUserBookings, cancelBooking, completeBooking } = require('../constrollers/booking.controller');
+const { verifyToken } = require('../middlewares/auth.middleware');
+const { isAdmin } = require('../middlewares/isAdmin.middleware');
+
+
+
+router.post('/:roomId', verifyToken, createBooking);
+
+router.get('/', isAdmin, getAllBookings);
+
+router.get('/user', verifyToken, getUserBookings)
+
+router.put('/cancel/:id', verifyToken, cancelBooking);
+
+router.put('/complete/:id', verifyToken, completeBooking)
+
+module.exports = router;
